0ceefe4b06c0f1dd7b90fc27199a3ef9c8bd8109
Alexis CRAYE <alexis1.craye@epitech.eu>
2017-11-08 13:36:32 +0100

    Adding Button class
    Adding class header source and adding source file to cmakelist.txt file

---
+++ lib/my_objects_lib/Button.cpp
@@ -0,0 +1,28 @@
+#include "Button.hpp"
+
+namespace my
+{
+	Button::Button()
+	{}
+
+	Button::~Button()
+	{}
+
+	/*bool my::Button::IsIntersect(const sf::Vector2f & point) const noexcept
+	{
+		if (m_sprite && m_sprite->IsIntersect(point))
+			return (true);
+		if (m_text && m_text->IsIntersect(point))
+			return (true);
+		return false;
+	}
+
+	bool my::Button::IsIntersect(const sf::IntRect & square) const noexcept
+	{
+		if (m_sprite && m_sprite->IsIntersect(square))
+			return (true);
+		if (m_text && m_text->IsIntersect(square))
+			return (true);
+		return false;
+	}*/
+}
